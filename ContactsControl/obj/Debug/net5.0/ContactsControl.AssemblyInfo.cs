e9d358d60781126a885cdfac1194a2c3012346fa
gabstabile <gabrielsstabile@outlook.com>
2024-11-09 14:03:21 -0300

    Create Front-end

--- ContactsControl/obj/Debug/net5.0/ContactsControl.AssemblyInfo.cs
+++ ContactsControl/obj/Debug/net5.0/ContactsControl.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactsControl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42759b88da25548a387f55dcddc85bec6f683b42")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactsControl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactsControl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
