fd16bf1d9f46091d43b0e756daca375f74bdd740
gabstabile <gabrielsstabile@outlook.com>
2024-11-09 13:45:04 -0300

    Initial commit

---
+++ ContactsControl/Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsControl.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
